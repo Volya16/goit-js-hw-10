d87dc5da82a00d881ae397eba0b38537260b54dc
Olya Vorontsova <16olya1996@gmail.com>
2025-01-11 01:29:01 +0100

    choose date

---
+++ src/js/1-timer.js
@@ -0,0 +1,41 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+let userSelectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+minuteIncrement: 1,
+locale: {
+        firstDayOfWeek: 1,
+        
+    },
+  onClose(selectedDates) { 
+      console.log(selectedDates[0]);
+      startBtnEl.disabled = true;
+      if (selectedDates[0] <= new Date()) {
+          alert('Please choose a date in the future');
+      } else {
+          startBtnEl.disabled = false;
+          userSelectedDate = selectedDates[0].getTime(); 
+          console.log(userSelectedDate);
+     }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+
+const inputEl = document.querySelector('.timer-input')
+const startBtnEl = document.querySelector('.timer-btn');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minEl = document.querySelector('[data-minutes]');
+const secEl = document.querySelector('[data-seconds]');
+
+// console.dir(inputEl);
+
+// let data = inputEl.value;
+// console.log(data);
